56298bc02c35fcfb338b6ecb77b0db9af4ac1b4d
Larry Hsiao <larryhsiao@larryhsiao.com>
2019-09-06 09:38:35 +0800

    tag list layout

--- app/src/main/java/com/larryhsiao/nyx/view/tag/TagListFragment.kt
+++ app/src/main/java/com/larryhsiao/nyx/view/tag/TagListFragment.kt
@@ -1,10 +1,21 @@
 package com.larryhsiao.nyx.view.tag
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.silverhetch.aura.AuraFragment
+import com.larryhsiao.nyx.R
 
 /**
  * Fragment of Tag list.
  */
 class TagListFragment : AuraFragment() {
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_tag_list, container, false)
+    }
 }
