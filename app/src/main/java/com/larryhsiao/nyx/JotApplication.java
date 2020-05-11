3b6a0def3c3ba0cbecadeafefd952108a05ee77f
Larry Hsiao <larryhsiao@larryhsiao.com>
2020-05-12 01:05:25 +0800

    Use firebase sync only.

--- app/src/main/java/com/larryhsiao/nyx/JotApplication.java
+++ app/src/main/java/com/larryhsiao/nyx/JotApplication.java
@@ -1,6 +1,7 @@
 package com.larryhsiao.nyx;
 
 import android.app.Application;
+import android.os.Build;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 import com.larryhsiao.nyx.core.NyxDb;
@@ -16,8 +17,6 @@ import java.sql.Connection;
  * Application of Jot.
  */
 public class JotApplication extends Application {
-    public static final String SKU_DRIVE_BACKUP = "cloud_backup";
-    public static final String SKU_PREMIUM = "premium";
     public static final String URI_FILE_PROVIDER = "content://com.larryhsiao.nyx.fileprovider/attachments/";
     public static final String URI_FILE_TEMP_PROVIDER = "content://com.larryhsiao.nyx.fileprovider/attachments_temp/";
     public long lastAuthed = 0L;
@@ -30,6 +29,9 @@ public class JotApplication extends Application {
         ContextHolder.setContext(this);
         File dbFile = new File(getFilesDir(), "jot");
         db = new SingleConn(new NyxDb(dbFile));
+        if ("robolectric".equals(Build.FINGERPRINT)) {
+            return;
+        }
         remoteConfig = FirebaseRemoteConfig.getInstance();
         remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults);
         remoteConfig.setConfigSettingsAsync(
