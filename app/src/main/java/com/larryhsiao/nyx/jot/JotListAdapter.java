38f46684337965eb92319de9443a2977738ea9fd
LarryHsiao <larryhsiao@larryhsiao.com>
2020-05-24 16:38:03 +0800

    UI improvement

--- app/src/main/java/com/larryhsiao/nyx/jot/JotListAdapter.java
+++ app/src/main/java/com/larryhsiao/nyx/jot/JotListAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.larryhsiao.nyx.R;
@@ -15,20 +16,31 @@ import com.larryhsiao.nyx.core.attachments.QueriedAttachments;
 import com.larryhsiao.nyx.core.jots.Jot;
 import com.silverhetch.aura.uri.UriMimeType;
 import com.silverhetch.aura.view.ViewHolder;
+import com.silverhetch.aura.view.measures.DP;
 import com.silverhetch.clotho.Source;
+import com.silverhetch.clotho.date.DateCalendar;
 
 import java.sql.Connection;
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static java.util.Calendar.SHORT;
+import static java.util.Calendar.SHORT_FORMAT;
+import static java.util.Calendar.getInstance;
+
 /**
  * Adapter for showing Jot list
  */
 public class JotListAdapter extends RecyclerView.Adapter<ViewHolder> {
+    private static final int ITEM_TOP = 1;
+    private static final int ITEM_MIDDLE = 2;
+    private static final int ITEM_END = 3;
     private final Source<Connection> db;
     private final List<Jot> data = new ArrayList<>();
     private final Function<Jot, Void> clicked;
@@ -76,6 +88,77 @@ public class JotListAdapter extends RecyclerView.Adapter<ViewHolder> {
         holder.getRootView().setOnClickListener(v -> clicked.apply(
             data.get(holder.getAdapterPosition()))
         );
+        int margin = ((int) new DP(holder.itemView.getContext(), 4).px());
+        RecyclerView.LayoutParams layoutParams = (RecyclerView.LayoutParams) holder.itemView.getLayoutParams();
+        switch (getItemViewType(position)) {
+            case ITEM_END:
+                layoutParams.bottomMargin = margin;
+                layoutParams.topMargin = 0;
+                holder.getTextView(R.id.itemJot_title).setText(
+                    SimpleDateFormat.getTimeInstance(DateFormat.SHORT).format(new Date(jot.createdTime()))
+                );
+                break;
+            case ITEM_MIDDLE:
+                layoutParams.topMargin = 0;
+                layoutParams.bottomMargin = 0;
+                holder.getTextView(R.id.itemJot_title).setText(
+                    SimpleDateFormat.getTimeInstance(DateFormat.SHORT).format(new Date(jot.createdTime()))
+                );
+                break;
+            case ITEM_TOP:
+            default:
+                layoutParams.topMargin = margin;
+                layoutParams.bottomMargin = 0;
+                if (position != data.size() - 1 &&
+                    new DateCalendar(jot.createdTime(), Calendar.getInstance()).value().equals(
+                        new DateCalendar(data.get(position + 1).createdTime(), Calendar.getInstance()).value()
+                    )
+                ) {
+                    holder.getTextView(R.id.itemJot_title).append(
+                        String.format(
+                            "\n%s",
+                            SimpleDateFormat.getTimeInstance(DateFormat.SHORT).format(new Date(jot.createdTime()))
+                        )
+                    );
+                }
+                break;
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return ITEM_TOP;
+        }
+
+        Calendar current = new DateCalendar(
+            data.get(position).createdTime(),
+            getInstance()
+        ).value();
+        Calendar previous = new DateCalendar(
+            data.get(position - 1).createdTime(),
+            getInstance()
+        ).value();
+
+        if (position == data.size() - 1) {
+            if (!current.equals(previous)) {
+                return ITEM_TOP;
+            } else {
+                return ITEM_END;
+            }
+        }
+
+        Calendar next = new DateCalendar(
+            data.get(position + 1).createdTime(),
+            getInstance()
+        ).value();
+        if (current.equals(previous) && current.equals(next)) {
+            return ITEM_MIDDLE;
+        } else if (current.equals(previous) && !current.equals(next)) {
+            return ITEM_END;
+        } else {
+            return ITEM_TOP;
+        }
     }
 
     @Override
