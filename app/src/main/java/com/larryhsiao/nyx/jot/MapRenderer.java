38c8d24f0d2ca150de221ef22bf887258445c958
Larry Hsiao <larryhsiao@larryhsiao.com>
2020-07-13 02:42:34 +0800

    Draw cluster preview at map

--- app/src/main/java/com/larryhsiao/nyx/jot/MapRenderer.java
+++ app/src/main/java/com/larryhsiao/nyx/jot/MapRenderer.java
@@ -1,14 +1,13 @@
 package com.larryhsiao.nyx.jot;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.graphics.*;
 import android.graphics.BitmapFactory.Options;
-import android.graphics.Matrix;
 import android.net.Uri;
 import androidx.annotation.Nullable;
 import androidx.exifinterface.media.ExifInterface;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.clustering.Cluster;
 import com.google.maps.android.clustering.ClusterManager;
@@ -23,7 +22,11 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.util.List;
 
+import static android.graphics.Bitmap.Config.ARGB_8888;
+import static android.graphics.Color.WHITE;
+import static android.graphics.PorterDuff.Mode.SRC_OVER;
 import static com.google.android.gms.maps.model.BitmapDescriptorFactory.fromBitmap;
+import static com.larryhsiao.nyx.JotApplication.URI_FILE_PROVIDER;
 import static java.util.stream.Collectors.toList;
 
 /**
@@ -33,6 +36,7 @@ public class MapRenderer extends DefaultClusterRenderer<JotMapItem> {
     private final Context context;
     private final Source<Connection> db;
     private int width = 0;
+    private int padding4 = 0;
 
     public MapRenderer(
         Context context,
@@ -51,15 +55,75 @@ public class MapRenderer extends DefaultClusterRenderer<JotMapItem> {
     }
 
     @Override
-    protected void onBeforeClusterItemRendered(JotMapItem item, MarkerOptions options) {
-        if (width == 0) {
-            width = ((int) new DP(context, 100).px());
+    protected void onBeforeClusterRendered(Cluster<JotMapItem> cluster, MarkerOptions options) {
+        generateDimensions();
+        if (cluster.getSize() > 0) {
+            for (JotMapItem item : cluster.getItems()) {
+                List<Attachment> content = new QueriedAttachments(
+                    new AttachmentsByJotId(db, item.getJot().id())
+                ).value()
+                    .stream()
+                    .filter(attachment -> attachment.uri().startsWith("content"))
+                    .limit(1)
+                    .collect(toList());
+                if (content.size() > 0) {
+                    try {
+                        Bitmap bitmap = iconBitmap(Uri.parse(content.get(0).uri()));
+                        if (bitmap != null) {
+                            options.icon(clusterIcon(cluster, bitmap));
+                        }
+                        break;
+                    } catch (Exception e) {
+                        super.onBeforeClusterRendered(cluster, options);
+                    }
+                }
+            }
+        } else {
+            super.onBeforeClusterRendered(cluster, options);
         }
+    }
+
+    private BitmapDescriptor clusterIcon(Cluster<JotMapItem> cluster, Bitmap bitmap) {
+        if (!bitmap.isMutable()) {
+            Bitmap tempBitmap = bitmap.copy(ARGB_8888, true);
+            bitmap.recycle();
+            bitmap = tempBitmap;
+        }
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        paint.setTextSize(35);
+        paint.setXfermode(new PorterDuffXfermode(SRC_OVER));
+        String indicator = String.valueOf(cluster.getItems().size());
+        Rect bounds = new Rect();
+        paint.getTextBounds(indicator, 0, indicator.length(), bounds);
+        paint.setColor(Color.parseColor("#BB000000"));
+        canvas.drawRoundRect(
+            bitmap.getWidth() - bounds.width() - (padding4 *2),
+            bitmap.getHeight() - bounds.height()- (padding4 *2),
+            bitmap.getWidth() ,
+            bitmap.getHeight(),
+            padding4,
+            padding4,
+            paint
+        );
+        paint.setColor(WHITE);
+        canvas.drawText(
+            indicator,
+            bitmap.getWidth() - bounds.width() - padding4,
+            bitmap.getHeight() - padding4,
+            paint
+        );
+        return fromBitmap(bitmap);
+    }
+
+    @Override
+    protected void onBeforeClusterItemRendered(JotMapItem item, MarkerOptions options) {
+        generateDimensions();
         List<Attachment> content = new QueriedAttachments(
             new AttachmentsByJotId(db, item.getJot().id())
         ).value()
             .stream()
-            .filter(attachment -> attachment.uri().startsWith("content"))
+            .filter(attachment -> attachment.uri().startsWith(URI_FILE_PROVIDER))
             .limit(1)
             .collect(toList());
         if (content.size() > 0) {
@@ -73,6 +137,15 @@ public class MapRenderer extends DefaultClusterRenderer<JotMapItem> {
             }
         }
         super.onBeforeClusterItemRendered(item, options);
+    }
+
+    private void generateDimensions() {
+        if (width == 0) {
+            width = ((int) new DP(context, 100).px());
+        }
+        if (padding4 == 0) {
+            padding4 = ((int) new DP(context, 4).px());
+        }
     }
 
     @Nullable
