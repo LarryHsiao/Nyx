e3c9393ce4b2f97fc02f52ef312e60616cfd4fdc
Larry Hsiao <larryhsiao@larryhsiao.com>
2020-08-29 02:18:37 +0800

    Back to empty

---
+++ app/src/main/java/com/larryhsiao/nyx/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.larryhsiao.nyx
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Entry Activity of Nyx.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/larryhsiao/nyx/jot/JotsFragment.java
@@ -0,0 +1,9 @@
+package com.larryhsiao.nyx.jot;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * Fragment that shows all Jots.
+ */
+public class JotsFragment extends Fragment {
+}
