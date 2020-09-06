4b4a90522f24031caa5f76b330bdd4f3c146eef2
Larry Hsiao <larryhsiao@larryhsiao.com>
2020-09-06 13:59:56 +0800

    poll field into viewModel of jot page

--- app/src/main/java/com/larryhsiao/nyx/jot/JotFragment.kt
+++ app/src/main/java/com/larryhsiao/nyx/jot/JotFragment.kt
@@ -9,15 +9,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
 import android.widget.TimePicker
+import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.larryhsiao.nyx.NyxFragment
 import com.larryhsiao.nyx.R
 import com.larryhsiao.nyx.ViewModelFactory
-import com.larryhsiao.nyx.core.jots.WrappedJot
 import kotlinx.android.synthetic.main.fragment_jot.*
-import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -25,7 +27,6 @@ import java.util.*
  * Fragment for representing a Jot.
  */
 class JotFragment : NyxFragment(), DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
-    private lateinit var mainScope: CoroutineScope
     private val dateFormatter by lazy {
         SimpleDateFormat("d MMM yyyy | hh:mm a", Locale.getDefault())
     }
@@ -57,6 +58,7 @@ class JotFragment : NyxFragment(), DatePickerDialog.OnDateSetListener, TimePicke
             }
         }
     }
+
     private val timePicker by lazy {
         val current = Calendar.getInstance()
         TimePickerDialog(
@@ -76,15 +78,11 @@ class JotFragment : NyxFragment(), DatePickerDialog.OnDateSetListener, TimePicke
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mainScope = MainScope()
         jot_title_right_textView.setOnClickListener { save() }
         jot_calendar_imageView.setOnClickListener { showDatePicker() }
         jot_clock_imageView.setOnClickListener { showTimePicker() }
-        jotViewModel.jot().observe(viewLifecycleOwner, {
-            jot_datetime_textView.text = formattedDate(it.createdTime())
-            jot_title_editText.setText(it.title())
-            jot_content_editText.setText(it.content())
-        })
+        jot_title_editText.doAfterTextChanged { jotViewModel.preferTitle(it?.toString() ?: "") }
+        jot_content_editText.doAfterTextChanged { jotViewModel.preferContent(it?.toString() ?: "") }
         jotViewModel.isNewJot().observe(viewLifecycleOwner, {
             jot_title_bar_title_textView.text = if (it) {
                 getString(R.string.New_Jot)
@@ -92,10 +90,13 @@ class JotFragment : NyxFragment(), DatePickerDialog.OnDateSetListener, TimePicke
                 getString(R.string.Edit)
             }
         })
+        jotViewModel.title().observe(viewLifecycleOwner, { jot_title_editText.setText(it) })
+        jotViewModel.content().observe(viewLifecycleOwner, { jot_content_editText.setText(it) })
+        jotViewModel.time().observe(viewLifecycleOwner, { jot_datetime_textView.text = formattedDate(it) })
         jotViewModel.loadJot(requireArguments().getLong("id"))
     }
 
-    private fun showDatePicker(){
+    private fun showDatePicker() {
         val currentDate = Calendar.getInstance().apply {
             time = dateFormatter.parse(jot_datetime_textView.text.toString()) ?: Date()
         }
@@ -107,7 +108,7 @@ class JotFragment : NyxFragment(), DatePickerDialog.OnDateSetListener, TimePicke
         datePicker.show()
     }
 
-    private fun showTimePicker(){
+    private fun showTimePicker() {
         val currentDate = Calendar.getInstance().apply {
             time = dateFormatter.parse(jot_datetime_textView.text.toString()) ?: Date()
         }
@@ -119,39 +120,20 @@ class JotFragment : NyxFragment(), DatePickerDialog.OnDateSetListener, TimePicke
     }
 
 
-    private fun save() = mainScope.launch {
+    private fun save() = lifecycleScope.launch {
         withContext(Default) {
-            jotViewModel.save(object : WrappedJot(jotViewModel.jot().value) {
-                override fun content(): String {
-                    return jot_content_editText.text.toString()
-                }
-
-                override fun title(): String {
-                    return jot_title_editText.text.toString()
-                }
-
-                override fun createdTime(): Long {
-                    return dateFormatter.parse(jot_datetime_textView.text.toString())?.time ?: 0L
-                }
-            })
+            jotViewModel.save()
             jotsViewModel.reload()
         }
         findNavController().popBackStack()
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        mainScope.cancel()
-    }
-
     private fun formattedDate(time: Long): String {
-        return dateFormatter.format(
-            Date(time)
-        )
+        return dateFormatter.format(Date(time))
     }
 
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-        jot_datetime_textView.text = formattedDate(Calendar.getInstance().apply {
+        jotViewModel.preferTime(Calendar.getInstance().apply {
             time = dateFormatter.parse(jot_datetime_textView.text.toString()) ?: Date()
             set(Calendar.YEAR, year)
             set(Calendar.MONTH, month)
@@ -160,7 +142,7 @@ class JotFragment : NyxFragment(), DatePickerDialog.OnDateSetListener, TimePicke
     }
 
     override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
-        jot_datetime_textView.text = formattedDate(Calendar.getInstance().apply {
+        jotViewModel.preferTime(Calendar.getInstance().apply {
             time = dateFormatter.parse(jot_datetime_textView.text.toString()) ?: Date()
             set(Calendar.HOUR_OF_DAY, hourOfDay)
             set(Calendar.MINUTE, minute)
--- app/src/main/java/com/larryhsiao/nyx/jot/JotViewModel.kt
+++ app/src/main/java/com/larryhsiao/nyx/jot/JotViewModel.kt
@@ -4,10 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.larryhsiao.nyx.core.jots.ConstJot
-import com.larryhsiao.nyx.core.jots.Jot
-import com.larryhsiao.nyx.core.jots.JotById
-import com.larryhsiao.nyx.core.jots.PostedJot
+import com.larryhsiao.nyx.core.jots.*
 import com.silverhetch.clotho.Source
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
@@ -18,8 +15,16 @@ import java.sql.Connection
  * ViewModel to representing a jot.
  */
 class JotViewModel(private val db: Source<Connection>) : ViewModel() {
-    private val jotLiveData = MutableLiveData<Jot>()
-    fun jot(): LiveData<Jot> = jotLiveData
+    private val time = MutableLiveData<Long>()
+    fun time(): LiveData<Long> = time
+
+    private val content = MutableLiveData<String>()
+    fun content(): LiveData<String> = content
+
+    private val title = MutableLiveData<String>()
+    fun title(): LiveData<String> = title
+
+    private val jot = MutableLiveData<Jot>()
 
     private val isNewJotLiveData = MutableLiveData<Boolean>()
     fun isNewJot(): LiveData<Boolean> = isNewJotLiveData
@@ -27,14 +32,43 @@ class JotViewModel(private val db: Source<Connection>) : ViewModel() {
     fun loadJot(id: Long) = viewModelScope.launch(IO) {
         if (id != -1L) {
             isNewJotLiveData.postValue(false)
-            jotLiveData.postValue(JotById(id, db).value())
+            loadContent(JotById(id, db).value())
         } else {
             isNewJotLiveData.postValue(true)
-            jotLiveData.postValue(ConstJot())
+            loadContent(ConstJot())
         }
     }
 
-    suspend fun save(jot: Jot): Jot = withContext(IO) {
-        PostedJot(db, jot).value()
+    private fun loadContent(constJot: Jot) = viewModelScope.launch {
+        jot.value = constJot
+        time.value = constJot.createdTime()
+        content.value = constJot.content()
+        title.value = constJot.title()
+    }
+
+    suspend fun save(): Jot = withContext(IO) {
+        PostedJot(db, object : WrappedJot(jot.value ?: ConstJot()) {
+            override fun content(): String = content.value ?: ""
+            override fun title(): String = title.value ?: ""
+            override fun createdTime(): Long = time.value ?: 0L
+        }).value()
+    }
+
+    fun preferTitle(newTitle: String) {
+        if (title.value == newTitle) {
+            return
+        }
+        title.value = newTitle
+    }
+
+    fun preferContent(newContent: String) {
+        if (content.value == newContent) {
+            return
+        }
+        content.value = newContent
+    }
+
+    fun preferTime(newTime: Long) {
+        time.value = newTime
     }
 }
