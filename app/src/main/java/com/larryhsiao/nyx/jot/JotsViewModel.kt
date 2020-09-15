d85bf57377bd85691d5866ee5598611f2e61c0c8
Larry Hsiao <larryhsiao@larryhsiao.com>
2020-09-15 22:02:38 +0800

    New Jot by images group by location and time(30 min)

--- app/src/main/java/com/larryhsiao/nyx/jot/JotsViewModel.kt
+++ app/src/main/java/com/larryhsiao/nyx/jot/JotsViewModel.kt
@@ -13,12 +13,18 @@ import com.larryhsiao.nyx.core.jots.JotsByDate
 import com.larryhsiao.nyx.core.jots.NewJot
 import com.larryhsiao.nyx.core.jots.QueriedJots
 import com.larryhsiao.nyx.core.jots.goemetry.MeterDelta
+import com.silverhetch.aura.images.exif.ExifAttribute
+import com.silverhetch.aura.images.exif.ExifUnixTimeStamp
+import com.silverhetch.clotho.source.ConstSource
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.sql.Date
 import java.util.*
+import kotlin.Double.Companion.MIN_VALUE
 import kotlin.collections.HashMap
+import kotlin.math.abs
+import kotlin.math.pow
 import kotlin.math.sqrt
 
 /**
@@ -61,28 +67,59 @@ class JotsViewModel(private val app: JotApplication) : ViewModel() {
 
     fun newJotsByImage(vararg uris: Uri) = viewModelScope.launch {
         loading.value = true
-        val jots = HashMap<DoubleArray, ArrayList<Uri>>()
+        val jots = HashMap<ExifInterface, ArrayList<Uri>>()
         uris.forEach { uri ->
-            ExifInterface(
-                app.contentResolver.openInputStream(uri) ?: return@forEach
-            ).latLong?.let { y ->
-                jots.keys.forEach existForEach@{ x ->
-                    // @todo #1 Accurate distance determination
-                    // @todo #2 Configurable distance range
-                    val distance = sqrt((x[0] - y[0]) * (x[0] - y[0]) + (x[1] - y[1]) * (x[1] - y[1]))
-                    if (distance < MeterDelta(30.0).value()) {
-                        jots[x] = jots[x]?.apply { add(uri) } ?: arrayListOf(uri)
-                        return@forEach
-                    }
+            val uriExif = ExifInterface(app.contentResolver.openInputStream(uri) ?: return@forEach)
+            val uriLatLong = uriExif.latLong ?: doubleArrayOf(MIN_VALUE, MIN_VALUE)
+            val uriTime = exifTime(uriExif)
+            jots.keys.forEach existForEach@{ existExif ->
+                val existLatLong = existExif.latLong
+                    ?: doubleArrayOf(MIN_VALUE, MIN_VALUE)
+                // @todo #1 Accurate distance determination
+                // @todo #2 Configurable distance range and time range
+                val distance = sqrt(
+                    (existLatLong[0] - uriLatLong[0]).pow(2.0) +
+                        (existLatLong[1] - uriLatLong[1]).pow(2.0)
+                )
+                if (distance < MeterDelta(30.0).value() &&
+                    abs(uriTime - exifTime(existExif)) < 30 * 60 * 1000 // 30 min
+                ) {
+                    jots[existExif] = jots[existExif]?.apply { add(uri) } ?: arrayListOf(uri)
+                    return@forEach
                 }
-                jots[y] = arrayListOf(uri)
             }
+            jots[uriExif] = arrayListOf(uri)
         }
         jots.forEach { it ->
-            val newJot = NewJot(app.db, "", "", it.key.reversedArray()).value()
+            val newJot = NewJot(
+                app.db,
+                "",
+                "",
+                it.key.latLong?.reversedArray() ?: doubleArrayOf(MIN_VALUE, MIN_VALUE),
+                Calendar.getInstance().apply { timeInMillis = exifTime(it.key) }
+            ).value()
             NewAttachments(app.db, newJot.id(), it.value.map { it.toString() }.toTypedArray()).value()
-            selectDate(selected.value ?: Calendar.getInstance())
         }
+        if (jots.size > 0) {
+            selectDate(Calendar.getInstance().apply {
+                timeInMillis = exifTime(jots.keys.first())
+            })}
         loading.value = false
+    }
+
+    private fun exifTime(exif: ExifInterface): Long {
+        return ExifUnixTimeStamp(
+            ExifAttribute(
+                ConstSource(exif),
+                ExifInterface.TAG_DATETIME_ORIGINAL
+            )
+        ).value().let {
+            if (it > 0) {
+                val tz = TimeZone.getDefault()
+                it - tz.getOffset(Calendar.ZONE_OFFSET.toLong())
+            } else {
+                Calendar.getInstance().timeInMillis
+            }
+        }
     }
 }
