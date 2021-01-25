cd8dfeafebb39dea0958f881f3a90e23b554044c
Larry Hsiao <larryhsiao@larryhsiao.com>
2021-01-25 10:48:18 +0800

    fix tests

--- core/src/test/java/com/larryhsiao/nyx/core/sync/server/TkJotsTest.java
+++ core/src/test/java/com/larryhsiao/nyx/core/sync/server/TkJotsTest.java
@@ -2,6 +2,7 @@ package com.larryhsiao.nyx.core.sync.server;
 
 import com.larryhsiao.clotho.Source;
 import com.larryhsiao.clotho.database.h2.MemoryH2Conn;
+import com.larryhsiao.nyx.core.jots.ConstJot;
 import com.larryhsiao.nyx.core.jots.JotsDb;
 import com.larryhsiao.nyx.core.jots.NewJot;
 import org.junit.jupiter.api.Assertions;
@@ -19,9 +20,15 @@ class TkJotsTest {
     @Test
     void getAll() throws IOException {
         Source<Connection> db = new JotsDb(new JotsDb(new MemoryH2Conn()));
-        new NewJot(db, "title", "content").value();
+        new NewJot(db, new ConstJot(
+            1, "title","content", 1L
+        )).value();
         Assertions.assertEquals(
-            "[{\"id\":1,\"title\":\"title\"}]",
+            "[{\"id\":1,\"title\":\"title\","
+                + "\"content\":\"content\","
+                + "\"deleted\":false,\"createdTime\":1,"
+                + "\"location\":[0.0,0.0],\"mood\":\"\","
+                + "\"privateLock\":false,\"version\":1}]",
             new RsPrint(
                 new TkJots(db).act(new RqFake("GET"))
             ).printBody()
