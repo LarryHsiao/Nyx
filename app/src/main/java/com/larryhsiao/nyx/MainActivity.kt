111d7fa3982eb9fff11a7c36b69a24d41bf77686
Larry Hsiao <larryhsiao@larryhsiao.com>
2021-06-12 21:36:04 +0800

    refactor: Remove all sync related code

--- app/src/main/java/com/larryhsiao/nyx/MainActivity.kt
+++ app/src/main/java/com/larryhsiao/nyx/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.larryhsiao.nyx
 
-import android.app.Service
-import android.content.ComponentName
-import android.content.Intent
-import android.content.ServiceConnection
 import android.os.Bundle
-import android.os.IBinder
 import androidx.appcompat.app.AppCompatActivity
-import com.larryhsiao.nyx.server.ServerService
 
 
 /**
  * Entry Activity of Nyx.
  */
-class MainActivity : AppCompatActivity(), ServiceConnection {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,24 +15,9 @@ class MainActivity : AppCompatActivity(), ServiceConnection {
 
     override fun onResume() {
         super.onResume()
-        bindService(
-            Intent(
-                this,
-                ServerService::class.java
-            ),
-            this,
-            Service.BIND_AUTO_CREATE
-        )
     }
 
     override fun onPause() {
         super.onPause()
-        unbindService(this)
-    }
-
-    override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-    }
-
-    override fun onServiceDisconnected(name: ComponentName?) {
     }
 }
